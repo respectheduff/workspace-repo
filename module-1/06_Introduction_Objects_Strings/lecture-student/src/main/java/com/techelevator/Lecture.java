53d3d1cee1ca816ea04cf14d61ba9d14be8bae2f
Tom Medvitz <tom@techelevator.com>
2021-09-20 12:42:15 -0400

    New material

--- module-1/06_Introduction_Objects_Strings/lecture-student/src/main/java/com/techelevator/Lecture.java
+++ module-1/06_Introduction_Objects_Strings/lecture-student/src/main/java/com/techelevator/Lecture.java
@@ -1,5 +1,8 @@
 package com.techelevator;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Lecture {
 
 	public static void main(String[] args) {
@@ -14,6 +17,8 @@ public class Lecture {
 		 * is not the case for most classes */
 
 		/* create an new instance of String using a literal */
+		String name = "Tom";
+		// String name = new String("Tom");
 
 		
 		System.out.println();
@@ -21,6 +26,57 @@ public class Lecture {
 		System.out.println("****** MEMBER METHODS ******");
 		System.out.println("******************************");
 		System.out.println();
+
+		//                   000000000011111111112222222222233333333334
+		//                   012345678901234567890123456789012345678901
+		String obligatory = "Hello World, You're World awesome World and I love you";
+		char d = obligatory.charAt(10);
+		String dString = obligatory.substring(10, 11);
+
+		char[] charArray = obligatory.toCharArray();
+
+
+		Scanner keyboard = new Scanner(System.in);
+
+		while(true) {
+			System.out.println("Enter (C)elsius or (F)ahrenheit >>> ");
+			String userChoice = keyboard.nextLine();
+			if (userChoice.toLowerCase().trim().startsWith("c")) {
+				break;
+			} else {
+				System.out.println("Try again!");
+				System.out.println(userChoice + " is not valid input.");
+			}
+		}
+
+		boolean flag = true;
+		for (int i = 0; i < 10 && flag; i++) {
+			for (int j = 0; j < 10; j++) {
+				if (j == i) {
+					flag = false;
+					break;
+				}
+			}
+		}
+
+		System.out.println(i);
+
+		// Immutable!
+		obligatory = obligatory.toUpperCase();
+		obligatory.startsWith("HELLO");
+
+
+		// "Hello World, You're World awesome World and I love you"
+		int firstWorld = obligatory.indexOf("World"); // 6
+		obligatory = obligatory.substring(firstWorld + 1);
+		// "orld, You're World awesome World and I love you"
+		int secondWorld = obligatory.indexOf("World"); // 13
+		int lastWorld = obligatory.lastIndexOf("World");
+
+
+		String garbage = "             Hello There Tom this is a long       sentence          ";
+		String slightlyLessGarbage = garbage.trim();
+		// "Hello There Tom this is a long       sentence"
 
 
 		/* Other commonly used methods:
@@ -46,13 +102,17 @@ public class Lecture {
 
 		/* Double equals will compare to see if the two variables, hello1 and
 		 * hello2 point to the same object in memory. Are they the same object? */
+
+		String hello1 = new String("Hello"); // 0xFACE
+		String hello2 = new String("Hello"); // 0xFADE
+
 		if (hello1 == hello2) {
 			System.out.println("They are equal!");
 		} else {
 			System.out.println(hello1 + " is not equal to " + hello2);
 		}
 
-		String hello3 = hello1;
+		String hello3 = hello1; // 0xFACE
 		if (hello1 == hello3) {
 			System.out.println("hello1 is the same reference as hello3");
 		}
@@ -64,6 +124,24 @@ public class Lecture {
 		} else {
 			System.out.println(hello1 + " is not equal to " + hello2);
 		}
+
+		//                 0123456789
+		String language = "JavaScript";
+		for (int i = 0; i < language.length(); i++) {
+			String thisLetter = language.substring(i, i+1);
+
+			System.out.println(thisLetter);
+		}
+
+		String example;  // example is null
+//		example.length(); // Null Pointer Exception
+
+		String example2 = "";
+		example2.length(); // 0
+
+
+
+
 
 	}
 }
