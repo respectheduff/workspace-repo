18d37df4ed0c206eb78b119b4cefdef2e782b717
Tom Medvitz <tom@techelevator.com>
2021-10-04 09:15:14 -0400

    New material

---
+++ module-1/16_FileIO_Reading_in/java/src/main/java/com/techelevator/exceptions/ExceptionsLecture.java
@@ -0,0 +1,11 @@
+package com.techelevator.exceptions;
+
+import java.util.Scanner;
+
+public class ExceptionsLecture {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+	}
+}
---
+++ module-1/16_FileIO_Reading_in/java/src/main/java/com/techelevator/scanner/RTNValidator.java
@@ -0,0 +1,22 @@
+package com.techelevator.scanner;
+
+public class RTNValidator {
+	
+	private static final int[] CHECKSUM_WEIGHTS = new int[] { 3, 7, 1, 3, 7, 1, 3, 7, 1 };
+
+	private void printApplicationBanner() {
+		System.out.println("******************");
+		System.out.println("RTN Validator 9000");
+		System.out.println("******************");
+		System.out.println();
+	}
+
+	private boolean checksumIsValid(String routingNumber) {
+		int checksum = 0;
+		for(int i = 0; i < 9; i++) {
+			int digit = Integer.parseInt(routingNumber.substring(i, i+1));
+			checksum += digit * CHECKSUM_WEIGHTS[i];
+		}
+		return checksum % 10 == 0;
+	}
+}
