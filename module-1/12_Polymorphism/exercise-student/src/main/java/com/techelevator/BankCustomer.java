2dc1b62cf81b1fae6d7b80a588f4ed6623fe6812
Joshua Littles <littles.j.i.i@gmail.com>
2021-10-08 00:15:00 -0400

    1 more down

--- module-1/12_Polymorphism/exercise-student/src/main/java/com/techelevator/BankCustomer.java
+++ module-1/12_Polymorphism/exercise-student/src/main/java/com/techelevator/BankCustomer.java
@@ -1,4 +1,32 @@
 package com.techelevator;
 
-public class BankCustomer {
+import java.util.List;
+
+public class BankCustomer implements Accountable{
+
+    private String name;
+    private String address;
+    private String phoneNumber;
+    public List<Accountable> accounts;
+    private int BankCustomer;
+
+
+    public int getBalance() {
+        return 0;
+    }
+
+    public void addAccount(Accountable newAccount){
+
+    }
+
+    public boolean isVip(){
+
+        if(BankCustomer >= 25000){
+            return true;
+        }
+
+
+        return false;
+    }
+
 }
--- module-1/16_FileIO_Reading_in/exercise-student/src/main/java/com/techelevator/QuizMaker.java
+++ module-1/16_FileIO_Reading_in/exercise-student/src/main/java/com/techelevator/QuizMaker.java
@@ -1,43 +1,43 @@
-package com.techelevator;
-
-public class QuizMaker {
-
-	public static void main(String[] args) {
-
-		UserInteraction ui = new UserInteraction();
-
-		File quizFile = new File("test_quiz.txt");
-		try{
-			Scanner quizFileScanner = new Scanner(quizFile);
-			List<QuizQuestion> questionList = new ArrayList<>();
-
-			while(quizFileStreamer.hjasnextLine()) {
-				String line = quizFileStreamer.nextLine();
-				QuizQuestion q = new QuizQuestion(line);
-				questionlist.add(q);
-			}
-
-			int correctAnswers = 0;
-			for(QuizQuestion q : questionList) {
-				ui.askQuizQuestion(q);
-				String guess = ui.promptUserForGuess();
-				if (q.isCorrectAnswer(guess)) {
-					System.out.println("You are correct!");
-					correctAnswers++;
-				} else {
-					System.out.println("You are wrong! ");
-				}
-			}
-			System.out.println();
-			System.out.println("you got" + correctAnswers "out of" + questionList.size() + "correct");
-			System.out.println(((double)correctAnswers * 10) / questionList.size() + "%");
-
-
-
-		}catch (FileNotFoundException e) {
-
-			QuizQuestion q = new QuizQuestion();
-		}
-	}
-
-}
+//package com.techelevator;
+//
+//public class QuizMaker {
+//
+//	public static void main(String[] args) {
+//
+//		UserInteraction ui = new UserInteraction();
+//
+//		File quizFile = new File("test_quiz.txt");
+//		try{
+//			Scanner quizFileScanner = new Scanner(quizFile);
+//			List<QuizQuestion> questionList = new ArrayList<>();
+//
+//			while(quizFileStreamer.hjasnextLine()) {
+//				String line = quizFileStreamer.nextLine();
+//				QuizQuestion q = new QuizQuestion(line);
+//				questionlist.add(q);
+//			}
+//
+//			int correctAnswers = 0;
+//			for(QuizQuestion q : questionList) {
+//				ui.askQuizQuestion(q);
+//				String guess = ui.promptUserForGuess();
+//				if (q.isCorrectAnswer(guess)) {
+//					System.out.println("You are correct!");
+//					correctAnswers++;
+//				} else {
+//					System.out.println("You are wrong! ");
+//				}
+//			}
+//			System.out.println();
+//			System.out.println("you got" + correctAnswers "out of" + questionList.size() + "correct");
+//			System.out.println(((double)correctAnswers * 10) / questionList.size() + "%");
+//
+//
+//
+//		}catch (FileNotFoundException e) {
+//
+//			QuizQuestion q = new QuizQuestion();
+//		}
+//	}
+//
+//}
