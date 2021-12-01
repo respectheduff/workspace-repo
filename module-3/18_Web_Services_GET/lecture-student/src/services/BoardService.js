9b13b2f52356f48d29867ea1d924a1daeb38ade5
Tom Medvitz <tom@techelevator.com>
2021-12-01 08:40:09 -0500

    New material

---
+++ module-3/18_Web_Services_GET/lecture-student/src/services/BoardService.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+const http = axios.create({
+  baseURL: "https://te-pgh-api.azurewebsites.net/api",
+  headers: {
+    common: {
+      APIKEY: '000000'
+    }
+  }
+});
+
+export default {
+
+  getBoards() {
+    return http.get('/boards');
+  },
+
+  getCards(boardID) {
+    return http.get(`/boards/${boardID}`)
+  },
+
+  getCard(boardID, cardID) {
+    return http.get(`/boards/${boardID}`).then((response) => {
+      const cards = response.data.cards;
+      return cards.find(card => card.id == cardID);
+    })
+  }
+
+}
