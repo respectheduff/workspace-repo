bace38973dff6627629847e7dea6b052e92d5ba0
Joshua Littles <littles.j.i.i@gmail.com>
2021-12-03 08:20:17 -0500

    late work

--- module-3/16_Vue_Routing/exercise-student - Copy/src/App.vue
+++ module-3/16_Vue_Routing/exercise-student - Copy/src/App.vue
@@ -1,11 +1,14 @@
 <template>
   <div id="app">
+    <nav>
+      <router-link v-bind:to="{ name: 'home' }">Home</router-link> |
+      <router-link v-bind:to="{ name: 'myBooks' }">My Books</router-link>
+    </nav>
     <router-view />
   </div>
 </template>
 
 <script>
-
 export default {
   name: 'app'
 }
---
+++ module-3/16_Vue_Routing/exercise-student - Copy/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <main>
+    <best-seller-list />
+    <new-releases-list />
+  </main>
+</template>
+
+<script>
+import BestSellerList from '@/components/BestSellerList';
+import NewReleasesList from '@/components/NewReleasesList';
+export default {
+  components: {
+    BestSellerList,
+    NewReleasesList
+  }
+}
+</script>
+
+<style>
+</style>
